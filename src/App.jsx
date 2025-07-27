41ec102d052556536fac2a742515897153c6348f
Diego Martinez <diegommart2004@gmail.com>
2025-07-27 06:00:45 -0400

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import mainLogo from '/logo.png'
 import './components/App.css'
+import Navbar from './components/Navbar/Navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={mainLogo} className="logo" alt="Martini's Pokelab logo" />
-        </a>
+        <Navbar />
+        {/* Other components */}
       </div>
+    
       <h1>Welcome to the Pokelab! Choose from a variety of Paldea's pokemon to create a team with.</h1>
+      
       <div className="card">
         <button>
           Forge your team
@@ -21,9 +22,11 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
   )
 }
